73de54ba018df57eb3967c0c6a6d7f5c4af1c4e2
BoomDev <willferrens@gmail.com>
2021-02-16 09:30:20 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -30,12 +30,12 @@ export default function Home() {
           </a>
 
           <a href="/#" className={styles.card}>
-            <h3>in metus vulputate</h3>
+            <h3>blandit cursus risus</h3>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. In fermentum posuere urna nec tincidunt praesent semper.</p>
           </a>
 
           <a href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app" className={styles.card}>
-            <h3>in metus vulputate</h3>
+            <h3>blandit cursus risus</h3>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. In fermentum posuere urna nec tincidunt praesent semper.</p>
           </a>
         </div>
